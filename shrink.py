f94faab5035a96ed126c7166109a4300afd8fec3
NeutrinoLiu <liubangya@gmail.com>
2023-01-11 02:55:33 -0600

    Create shrink.py

---
+++ shrink.py
@@ -0,0 +1,23 @@
+import json
+
+# print(sys.argv)
+respFile = "resp.json"
+targetName = "shrank.json"
+with open(respFile, "r") as f:
+    rawRank = json.load(f)
+
+shrank = []
+for item in rawRank:
+    # print(item)
+    # shrank.append(
+    #     {   
+    #         # "rank" : item["rank"],
+    #         "id" : item["id"],
+    #         "sciRank" : item["scientificRank"]["sciRank"] if item["scientificRank"] else None,
+    #     })
+    shrank.append(
+        item["scientificRank"]["sciRank"] if item["scientificRank"] else None,
+        )
+    
+with open(targetName, "w+") as f:
+    f.writelines(json.dumps(shrank))
